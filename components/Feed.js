13c6eafc29781fc2a50663f089acef05ae604e8e
Aditya <akaditya394@gmail.com>
2022-06-10 14:59:00 +0530

    feed page done

--- components/Feed.js
+++ components/Feed.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Stories from "./Stories";
 import Posts from "./Posts";
+import MiniProfile from "./MiniProfile";
+import Suggestions from "./Suggestions";
 
 function Feed() {
   return (
@@ -9,7 +11,12 @@ function Feed() {
         <Stories />
         <Posts />
       </section>
-      <section></section>
+      <section className="hidden xl:inline-grid md:col-span-1 ">
+        <div className="fixed top-20">
+          <MiniProfile />
+          <Suggestions />
+        </div>
+      </section>
     </main>
   );
 }
